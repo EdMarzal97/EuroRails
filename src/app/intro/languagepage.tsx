4d4d3a7190ce8043851d791ee915d77eeef87bab
Edward Marzal <edsanchezmarzal@gmail.com>
2023-05-26 09:35:10 -0500

    title

--- src/app/intro/languagepage.tsx
+++ src/app/intro/languagepage.tsx
@@ -14,7 +14,7 @@ const IntroductionPage: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-8">Welcome to the App</h1>
+      <h1 className="text-4xl font-bold mb-8">Welcome to the Euro Rail Connect</h1>
       <p className="text-xl mb-4">Please select your preferred language:</p>
       <div className="flex space-x-4">
         <button
